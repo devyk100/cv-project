05e816d31f1b778857e8a7504d218c313ad7c4e6
SatvikKarri <sk23ecb1a03@student.nitw.ac.in>
2026-03-07 21:56:02 +0530

    Add files via upload
    Some examples of the preprocessed files are uploaded in this branch along with the code with converts the dataset into preprocessed images .

---
+++ capture_image.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+
+import cv2
+import os
+
+dataset = "/home/satvik/Desktop/lowlight_env/ExDark/"
+output_folder = "/home/satvik/Desktop/lowlight_env/preprocessed_images/"
+
+for category in os.listdir(dataset):
+
+    category_path = os.path.join(dataset, category)
+
+    if not os.path.isdir(category_path):
+        continue
+
+    for file in os.listdir(category_path):
+
+        path = os.path.join(category_path, file)
+
+        image = cv2.imread(path)
+
+        if image is None:
+            continue
+
+        resized = cv2.resize(image, (224,224))
+
+        ycrcb = cv2.cvtColor(resized, cv2.COLOR_BGR2YCrCb)
+
+        Y, Cr, Cb = cv2.split(ycrcb)
+
+        output_path = os.path.join(output_folder, "Y_" + file)
+
+        cv2.imwrite(output_path, Y)
+
+print("All images processed and saved.")
